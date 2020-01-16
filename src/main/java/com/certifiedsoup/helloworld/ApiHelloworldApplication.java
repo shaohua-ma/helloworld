4d29fafe0e906e723e327bfe78d1170701771fa5
masnl <masnl>
2020-01-16 04:16:24 -0800

    first working vesion.

---
+++ src/main/java/com/certifiedsoup/helloworld/ApiHelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.certifiedsoup.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiHelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiHelloworldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/certifiedsoup/helloworld/ApiHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.certifiedsoup.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiHelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
